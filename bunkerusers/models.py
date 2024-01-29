7f8c9e7dd3184bcf1b0f928979e1a94f742fd8c7
Fedot_Compot <shurfed25@gmal.com>
2024-01-29 21:17:08 +0100

    feat: updated colum names

--- bunkerusers/models.py
+++ bunkerusers/models.py
@@ -5,15 +5,15 @@ from django.contrib.sessions.models import Session
 
 # Create your models here.
 class User(models.Model):
-    game_id = models.ForeignKey(Game,on_delete=models.CASCADE)
+    game = models.ForeignKey(Game,on_delete=models.CASCADE)
     username = models.CharField(max_length=32)
-    session_key = models.ForeignKey(Session, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
     host = models.BooleanField(default=False)
     ready = models.BooleanField(default=False)
-    data =  models.JSONField(blank=True, null=True)
-    
+    data = models.JSONField(blank=True, null=True)
+
     class Meta:
         unique_together = (
-            ('game_id', 'username'),
-            ('game_id', 'session_key')
+            ('game', 'username'),
+            ('game', 'session')
         )
